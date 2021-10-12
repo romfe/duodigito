8f97b5f351f1dfd8668eb952a269541bd9bb5200
Rômulo <romfe89@gmail.com>
2021-10-12 10:20:21 -0300

    adicionada rotina para lidar com erros

--- back_end/src/main/java/romulo/springbootbackend/CalculoController.java
+++ back_end/src/main/java/romulo/springbootbackend/CalculoController.java
@@ -25,7 +25,7 @@ public class CalculoController {
         double menorMultiploDuodigito = 0.0; // variável que irá conter a solução do problema
 
         int multiplicador = 2;
-        int quantidadeDeDigitosDiferentes;
+        int quantidadeDeDigitosDiferentes; // contador de dígitos diferentes
 
         String numeroString = "";
         char primeiroDigito;
@@ -38,40 +38,44 @@ public class CalculoController {
 
             quantidadeDeDigitosDiferentes = 1;
 
-            novoMultiplo = multiplicador * numeroEntrada; // cálculo do múltiplo seguinte
-            numeroString = Double.toString(novoMultiplo).replace(".","");
+            try {
+                novoMultiplo = multiplicador * numeroEntrada; // cálculo do múltiplo seguinte
+                numeroString = Double.toString(novoMultiplo).replace(".", "");
 
-            // condicional para remover o último zero depois da vírgula, caso exista
-            if(numeroString.charAt(numeroString.length() - 1) == '0'){
-                numeroString = numeroString.substring(0, numeroString.length() - 1);
-            }
-
-            primeiroDigito = numeroString.charAt(0);
-
-
-            // estrutura de repetiçao que conta o número de algarismos diferentes no múltiplo
-            for(int i = 1; i < numeroString.length(); i++){
-                if(numeroString.charAt(i) != primeiroDigito){
-                    quantidadeDeDigitosDiferentes++;
+                // condicional para remover o último zero depois da vírgula, caso exista
+                if (numeroString.charAt(numeroString.length() - 1) == '0') {
+                    numeroString = numeroString.substring(0, numeroString.length() - 1);
                 }
+
+                primeiroDigito = numeroString.charAt(0);
+
+
+                // estrutura de repetiçao que conta o número de algarismos diferentes no múltiplo
+                for (int i = 1; i < numeroString.length(); i++) {
+                    if (numeroString.charAt(i) != primeiroDigito) {
+                        quantidadeDeDigitosDiferentes++;
+                    }
+                }
+
+                // caso o múltiplo seja duodígito, atribui-se o resultado à variável menorMultiploDuodigito
+                if (quantidadeDeDigitosDiferentes <= 2) {
+                    menorMultiploDuodigito = novoMultiplo;
+                    solucaoEncontrada = true;
+                }
+
+                // aumenta o multiplicador para calcular o próximo múltiplo
+                multiplicador++;
+
+                // para evitar que o programa rode indefinidamente
+                if (multiplicador > 1000000) return new Calculo(0.0, 0);
+
+            }catch(Exception err){
+                return new Calculo(0.0, 0);
             }
-
-            // caso o múltiplo seja duodígito, atribui-se o resultado à variável menorMultiploDuodigito
-            if(quantidadeDeDigitosDiferentes <= 2){
-                menorMultiploDuodigito = novoMultiplo;
-                solucaoEncontrada = true;
-            }
-
-            // aumenta o multiplicador para calcular o próximo múltiplo
-            multiplicador++;
-
-            // para evitar que o programa rode indefinidamente
-            if(multiplicador > 1000000) return new Calculo(0.0,0);
-
         }
 
 
-        long tempoExecucao = tempoExecucao(System.nanoTime(), horaComeco); // método do cálculo do tempo de execução
-        return new Calculo(menorMultiploDuodigito, tempoExecucao);
+        long tempo = tempoExecucao(System.nanoTime(), horaComeco); // método do cálculo do tempo de execução
+        return new Calculo(menorMultiploDuodigito, tempo);
     }
 }
